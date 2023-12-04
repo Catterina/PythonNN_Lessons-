7359c0d9cb6aba13d97fe3889801138291e77ac2
Katty <kattyivrn@gmail.com>
2023-12-04 08:02:42 +0300

    Добавлен lesson 21

---
+++ Lesson 21/constructors.py
@@ -0,0 +1,30 @@
+print(bool('ASD'))
+print(bool(''))
+print(bool(10))
+print(bool(0))
+print('-'*100)
+
+print(5**50)
+print(pow(5,50))
+print('-'*100)
+
+print(abs(-1))
+print(abs(1))
+print('-'*100)
+
+print(round(4.6666, 2))
+print(round(4.4444, 2))
+print('-'*100)
+
+list1 = [1, 2, 3]
+print(sum(list1))
+
+var_int = 0
+for i in list1:
+    var_int +=i
+print(var_int)
+
+
+print(min(2,4))
+print(max(2,4))
+
